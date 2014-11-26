00fcf72672f1d1b76c03949c33dc75673ce4328b
christineklow <clow@bu.edu>
2014-11-26 14:25:32 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/awesomeness/christine/ec327project/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.awesomeness.christine.ec327project;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
