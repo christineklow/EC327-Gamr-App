4f59843556a8a441c7ea66da4cbc996ba714c493
christineklow <clow@bu.edu>
2014-12-06 19:09:50 -0500

    tictactoe

--- app/src/main/java/com/awesomeness/christine/ec327project/tictactoe.java
+++ app/src/main/java/com/awesomeness/christine/ec327project/tictactoe.java
@@ -7,6 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class tictactoe extends Activity {
@@ -20,21 +23,312 @@ public class tictactoe extends Activity {
     ImageButton tic_7;
     ImageButton tic_8;
     ImageButton tic_9;
+    TextView wintext;
+    int[] gameboard = {1,2,3,4,5,6,7,8,9};
+    int computerinput=8;
+    int x,y,z;
+    int turns = 1;
+    int winstatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tictactoe);
-        configurebutton1();
-        configurebutton2();
-        configurebutton3();
-        configurebutton4();
-        configurebutton5();
-        configurebutton6();
-        configurebutton7();
-        configurebutton8();
-        configurebutton9();
+
+        do {
+            playerturn();
+            turns++;
+            computerturn(computerinput);
+            if (checkwin()) {
+                //player won
+                playerwon(x, y, z);
+                winstatus=1;
+                break;
+            }
+            computerinput = computerinput - 2;
+            turns++;
+            if (checkwin()) {
+                //computer won
+                computerwon(x, y, z);
+                winstatus=2;
+                break;
+            }
+
+            if(!checkwin() && turns==9){
+                winstatus=3; //tie
+            }
+
+        }while(!checkwin() || turns<10);
+
+
+        wintext=(TextView) findViewById(R.id.tictactoestatus);
+        switch(winstatus){
+            case 1: wintext.setText("You Won!");//player won
+            case 2: wintext.setText("Haha, you can't beat a computer");//computer won
+            case 3: wintext.setText("Well this is awkward.");//tie
+        }
+
     }
+
+    public void playerturn(){
+        for(int i = 1 ; i <10 ; i++){
+            switch (i){
+                case 1:{
+                    if (gameboard[i] == i){
+                        configurebutton1();
+                    }
+                }
+                case 2: {
+                    if (gameboard[i] == i) {
+                        configurebutton2();
+                    }
+                }
+                case 3: {
+                    if (gameboard[i] == i) {
+                        configurebutton3();
+                    }
+                }
+                case 4: {
+                    if (gameboard[i] == i) {
+                        configurebutton4();
+                    }
+                }
+                case 5: {
+                    if (gameboard[i] == i) {
+                        configurebutton5();
+                    }
+                }
+                case 6: {
+                    if (gameboard[i] == i) {
+                        configurebutton6();
+                    }
+                }
+                case 7: {
+                    if (gameboard[i] == i) {
+                        configurebutton7();
+                    }
+                }
+                case 8: {
+                    if (gameboard[i] == i) {
+                        configurebutton8();
+                    }
+                }
+                case 9: {
+                    if (gameboard[i] == i) {
+                        configurebutton9();
+                    }
+                }
+            }
+        }
+    }
+
+    //computer turn
+    public void computerturn(int turn){
+        int[] availablespots=new int[turn];
+        int count = 0;
+        for(int i = 0 ; i <9 ; i++) {
+            if (gameboard[i] != 0 && gameboard[i] != 10){
+                availablespots[count] = gameboard[i];
+                count++;
+            }
+        }
+
+        Random random=new Random();
+        int choice= random.nextInt(turn)+1;
+
+        int place = availablespots[choice];
+
+        switch(place){
+            case 1:{
+                tic_1.setImageResource(R.drawable.ocorner);
+                tic_1.setEnabled(false);
+                gameboard[1]=10;
+            }
+            case 2:{
+                tic_2.setImageResource(R.drawable.otwoandeight);
+                tic_2.setEnabled(false);
+                gameboard[2]=10;
+            }
+            case 3:{
+                tic_3.setImageResource(R.drawable.ocorner);
+                tic_3.setEnabled(false);
+                gameboard[3]=10;
+            }
+            case 4:{
+                tic_4.setImageResource(R.drawable.ofour);
+                tic_4.setEnabled(false);
+                gameboard[4]=10;
+            }
+            case 5:{
+                tic_5.setImageResource(R.drawable.ofive);
+                tic_5.setEnabled(false);
+                gameboard[5]=10;
+            }
+            case 6:{
+                tic_6.setImageResource(R.drawable.osix);
+                tic_6.setEnabled(false);
+                gameboard[6]=10;
+            }
+            case 7:{
+                tic_7.setImageResource(R.drawable.ocorner);
+                tic_7.setEnabled(false);
+                gameboard[7]=10;
+            }
+            case 8:{
+                tic_8.setImageResource(R.drawable.otwoandeight);
+                tic_8.setEnabled(false);
+                gameboard[8]=10;
+            }
+            case 9:{
+                tic_9.setImageResource(R.drawable.ocorner);
+                tic_9.setEnabled(false);
+                gameboard[9]=10;
+            }
+        }
+
+    }
+
+    //check if anybody won
+    public boolean checkwin(){
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+
+        boolean check = false;
+
+        if(gameboard[1] == gameboard[2] && gameboard[1] == gameboard[3])
+        {
+            x=1;
+            y=2;
+            z=3;
+            check = true;
+        }
+        else if(gameboard[1] == gameboard[4] && gameboard[1] == gameboard[7])
+        {
+            x=1;
+            y=4;
+            z=7;
+            check = true;
+        }
+        else if(gameboard[1] == gameboard[5] && gameboard[1] == gameboard[9])
+        {
+            x=1;
+            y=5;
+            z=9;
+            check = true;
+        }
+        else if(gameboard[4] == gameboard[5] && gameboard[4] == gameboard[6])
+        {
+            x=4;
+            y=5;
+            z=6;
+            check = true;
+        }
+        else if(gameboard[7] == gameboard[8] && gameboard[7] == gameboard[9])
+        {
+            x=7;
+            y=8;
+            z=9;
+            check = true;
+        }
+        else if(gameboard[2] == gameboard[5] && gameboard[2] == gameboard[8])
+        {
+            x=2;
+            y=5;
+            z=8;
+            check = true;
+        }
+        else if(gameboard[3] == gameboard[6] && gameboard[3] == gameboard[9])
+        {
+            x=3;
+            y=6;
+            z=9;
+            check = true;
+        }
+        else if(gameboard[3] == gameboard[5] && gameboard[3] == gameboard[7])
+        {
+            x=3;
+            y=5;
+            z=7;
+            check = true;
+        }
+
+        return check;
+    }
+
+    public void playerwon(int first, int second, int last){
+        switch (first){
+            case 1: tic_1.setImageResource(R.drawable.xcornerwin);
+            case 2: tic_2.setImageResource(R.drawable.xtwoandeigthwin);
+            case 3: tic_3.setImageResource(R.drawable.xcornerwin);
+            case 4: tic_4.setImageResource(R.drawable.xfourwin);
+            case 5: tic_5.setImageResource(R.drawable.xfivewin);
+            case 6: tic_6.setImageResource(R.drawable.xsixwin);
+            case 7: tic_7.setImageResource(R.drawable.xcornerwin);
+            case 8: tic_8.setImageResource(R.drawable.xtwoandeigthwin);
+            case 9: tic_9.setImageResource(R.drawable.xcornerwin);
+        }
+        switch (second){
+            case 1: tic_1.setImageResource(R.drawable.xcornerwin);
+            case 2: tic_2.setImageResource(R.drawable.xtwoandeigthwin);
+            case 3: tic_3.setImageResource(R.drawable.xcornerwin);
+            case 4: tic_4.setImageResource(R.drawable.xfourwin);
+            case 5: tic_5.setImageResource(R.drawable.xfivewin);
+            case 6: tic_6.setImageResource(R.drawable.xsixwin);
+            case 7: tic_7.setImageResource(R.drawable.xcornerwin);
+            case 8: tic_8.setImageResource(R.drawable.xtwoandeigthwin);
+            case 9: tic_9.setImageResource(R.drawable.xcornerwin);
+        }
+        switch (last){
+            case 1: tic_1.setImageResource(R.drawable.xcornerwin);
+            case 2: tic_2.setImageResource(R.drawable.xtwoandeigthwin);
+            case 3: tic_3.setImageResource(R.drawable.xcornerwin);
+            case 4: tic_4.setImageResource(R.drawable.xfourwin);
+            case 5: tic_5.setImageResource(R.drawable.xfivewin);
+            case 6: tic_6.setImageResource(R.drawable.xsixwin);
+            case 7: tic_7.setImageResource(R.drawable.xcornerwin);
+            case 8: tic_8.setImageResource(R.drawable.xtwoandeigthwin);
+            case 9: tic_9.setImageResource(R.drawable.xcornerwin);
+        }
+    }
+
+    public void computerwon(int first, int second, int last){
+        switch (first){
+            case 1: tic_1.setImageResource(R.drawable.ocornerwin);
+            case 2: tic_2.setImageResource(R.drawable.otwoandeightwin);
+            case 3: tic_3.setImageResource(R.drawable.ocornerwin);
+            case 4: tic_4.setImageResource(R.drawable.ofourwin);
+            case 5: tic_5.setImageResource(R.drawable.ofivewin);
+            case 6: tic_6.setImageResource(R.drawable.osixwin);
+            case 7: tic_7.setImageResource(R.drawable.ocornerwin);
+            case 8: tic_8.setImageResource(R.drawable.otwoandeightwin);
+            case 9: tic_9.setImageResource(R.drawable.ocornerwin);
+        }
+        switch (second){
+            case 1: tic_1.setImageResource(R.drawable.ocornerwin);
+            case 2: tic_2.setImageResource(R.drawable.otwoandeightwin);
+            case 3: tic_3.setImageResource(R.drawable.ocornerwin);
+            case 4: tic_4.setImageResource(R.drawable.ofourwin);
+            case 5: tic_5.setImageResource(R.drawable.ofivewin);
+            case 6: tic_6.setImageResource(R.drawable.osixwin);
+            case 7: tic_7.setImageResource(R.drawable.ocornerwin);
+            case 8: tic_8.setImageResource(R.drawable.otwoandeightwin);
+            case 9: tic_9.setImageResource(R.drawable.ocornerwin);
+        }
+        switch (last){
+            case 1: tic_1.setImageResource(R.drawable.ocornerwin);
+            case 2: tic_2.setImageResource(R.drawable.otwoandeightwin);
+            case 3: tic_3.setImageResource(R.drawable.ocornerwin);
+            case 4: tic_4.setImageResource(R.drawable.ofourwin);
+            case 5: tic_5.setImageResource(R.drawable.ofivewin);
+            case 6: tic_6.setImageResource(R.drawable.osixwin);
+            case 7: tic_7.setImageResource(R.drawable.ocornerwin);
+            case 8: tic_8.setImageResource(R.drawable.otwoandeightwin);
+            case 9: tic_9.setImageResource(R.drawable.ocornerwin);
+        }
+    }
+
 
     public void configurebutton1(){
         tic_1 = (ImageButton) findViewById(R.id.tic1);
@@ -43,6 +337,7 @@ public class tictactoe extends Activity {
             public void onClick(View v) {
                 tic_1.setImageResource(R.drawable.xcorner);
                 tic_1.setEnabled(false);
+                gameboard[1]=0;
             }
         });
     }
@@ -53,6 +348,8 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_2.setImageResource(R.drawable.xtwoandeigth);
+                tic_2.setEnabled(false);
+                gameboard[2]=0;
             }
         });
     }
@@ -63,6 +360,8 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_3.setImageResource(R.drawable.xcorner);
+                tic_3.setEnabled(false);
+                gameboard[3]=0;
             }
         });
     }
@@ -73,16 +372,21 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_4.setImageResource(R.drawable.xfour);
+                tic_4.setEnabled(false);
+                gameboard[4]=0;
             }
         });
     }
 
+    //player buttons
     public void configurebutton5(){
         tic_5 = (ImageButton) findViewById(R.id.tic5);
         tic_5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tic_5.setImageResource(R.drawable.xfive);
+                tic_5.setEnabled(false);
+                gameboard[5]=0;
             }
         });
     }
@@ -93,6 +397,8 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_6.setImageResource(R.drawable.xsix);
+                tic_6.setEnabled(false);
+                gameboard[6]=0;
             }
         });
     }
@@ -103,6 +409,8 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_7.setImageResource(R.drawable.xcorner);
+                tic_7.setEnabled(false);
+                gameboard[7]=0;
             }
         });
     }
@@ -113,6 +421,8 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_8.setImageResource(R.drawable.xtwoandeigth);
+                tic_8.setEnabled(false);
+                gameboard[8]=0;
             }
         });
     }
@@ -123,6 +433,8 @@ public class tictactoe extends Activity {
             @Override
             public void onClick(View v) {
                 tic_9.setImageResource(R.drawable.xcorner);
+                tic_9.setEnabled(false);
+                gameboard[9]=0;
             }
         });
     }
