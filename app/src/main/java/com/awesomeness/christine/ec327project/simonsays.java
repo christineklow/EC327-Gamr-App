5c45028ef572017ec18eb27c418e6095e2a5d897
christineklow <clow@bu.edu>
2014-12-07 03:59:27 -0500

    simon says

--- app/src/main/java/com/awesomeness/christine/ec327project/simonsays.java
+++ app/src/main/java/com/awesomeness/christine/ec327project/simonsays.java
@@ -4,14 +4,97 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Random;
 
 
 public class simonsays extends ActionBarActivity {
+
+    ImageButton green;
+    ImageButton blue;
+    ImageButton yellow;
+    ImageButton red;
+    TextView message;
+    int[] Pattern= new int[20];
+    int[] userInput= new int[20];
+    int count = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simonsays);
+
+        green = (ImageButton)findViewById(R.id.green); //1
+        blue = (ImageButton)findViewById(R.id.blue); //2
+        yellow = (ImageButton)findViewById(R.id.yellow); //3
+        red = (ImageButton)findViewById(R.id.red); //4
+        message = (TextView)findViewById(R.id.simoninstructions);
+
+        addpattern();
+    }
+
+    public void addpattern(){
+
+        //create next pattern step
+        Random random = new Random();
+        Pattern[count] = random.nextInt(3)+1;
+        green.setEnabled(false);
+        blue.setEnabled(false);
+        yellow.setEnabled(false);
+        red.setEnabled(false);
+
+        count++;
+
+        //show the pattern
+        for( int i = 0 ; i < count ; i++)
+        {
+            showpattern(Pattern[i]);
+        }
+
+        playerturn();
+    }
+
+    public void showpattern(int color){
+        switch(color){
+            case 1: green.setImageResource(R.drawable.simongreen);break;
+            case 2: blue.setImageResource(R.drawable.simonblue); break;
+            case 3: yellow.setImageResource(R.drawable.simonyellow);break;
+            case 4: red.setImageResource(R.drawable.simonred);break;
+        }
+    }
+
+    public boolean playerturn()
+    {
+        message.setText("Your turn.");
+        green.setEnabled(true);
+        blue.setEnabled(true);
+        yellow.setEnabled(true);
+        red.setEnabled(true);
+        return true;
+
+    }
+
+    public void configuregreen(){
+        green.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        green.setImageResource(R.drawable.simongreen);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        green.setImageResource(R.drawable.simongreenoff);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
 
